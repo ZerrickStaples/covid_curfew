a64fa4c5dce94dc2fa31faed1911ea5803f1a46f
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-12-13 13:03:29 -0500

    Installed moment.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './covidparticle.png';
+import moment from 'moment';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
     </div>
   );
 }
+
+
 
 export default App;
